8fd5593e89838fff726784360f46df6f5cc4c828
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-10-02 11:12:40 -0400

    almost done

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -23,15 +23,46 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
 
-// Show where the origin is
-canvas.drawAxes()
+//Ears
+canvas.drawEllipse(centreX: 90, centreY: 370, width: 70, height: 70)
+canvas.drawEllipse(centreX: 210, centreY: 370, width: 70, height: 70)
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+//Head
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 150, centreY: 290, width: 200, height: 200)
+
+//Eyes
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 110, centreY: 300, width: 55, height: 50)
+canvas.drawEllipse(centreX: 190, centreY: 300, width: 55, height: 50)
+
+
+
+//Eye dots
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 180, centreY: 310, width: 10, height: 10)
+canvas.drawEllipse(centreX: 120, centreY: 310, width: 10, height: 10)
+
+
+//Red spots
+canvas.fillColor = Color(hue: 350, saturation: 80, brightness: 75, alpha: 60)
+canvas.drawShapesWithBorders = false
+canvas.drawEllipse(centreX: 100, centreY: 260, width: 30, height: 20)
+canvas.drawEllipse(centreX: 200, centreY: 260, width: 30, height: 20)
+
+//Nose lines
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 140, centreY: 250, width: 30, height: 30)
+canvas.drawEllipse(centreX: 160, centreY: 250, width: 30, height: 30)
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 135, centreY: 250, width: 27, height: 25)
+canvas.drawEllipse(centreX: 165, centreY: 250, width: 27, height: 25)
+
+
+//Nose
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 150, centreY: 260, width: 30, height: 15)
 
 
 // This code is necessary to see the result in the Assistant Editor at right
